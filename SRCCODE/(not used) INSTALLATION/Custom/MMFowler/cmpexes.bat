d17137d88f022fdfe58773e191f3d6e4413fb1ab
Davidc <davidc@localhost>
1999-08-26 19:14:25 +0000

    vss-git-convert

---
+++ SRCCODE/(not used) INSTALLATION/Custom/MMFowler/cmpexes.bat
@@ -0,0 +1,40 @@
+rem Compile ONLY the EXE files
+@echo off
+
+if "%1" == "" goto parm_missing
+
+echo Delete z files
+del ..\disk\factbin.z
+del ..\disk\factdb.z
+
+echo .
+echo Compress Application files
+
+echo .
+echo call DISKBILD.BAT
+call buildlog.bat %1
+
+echo .
+echo Compress all files in appfiles directory to factbin.z
+icomp ..\appfiles\*.* ..\disk\factbin.z -i
+
+echo .
+echo Compress Local Database
+icomp I:\program\release\exectrak\local_db\*.* ..\disk\factdb.z
+
+rem echo .
+rem echo Run the PACKCALC and edit the SPLITZ.BAT/SETUP.LST
+rem echo and then RUN the SPLITZ.BAT
+
+echo .
+echo RUN the SPLITZ.BAT
+call splitz
+
+goto finished
+
+:parm_missing
+echo .
+echo missing or invalid parameter.  format: cmpfiles [Module Version]
+echo .
+
+:finished
---
+++ SRCCODE/(not used) INSTALLATION/Custom/MMFowler/splitz.bat
@@ -0,0 +1,51 @@
+@echo off
+call makedir.bat
+
+echo split factbin.z
+call fsplit ..\disk\factbin.z ..\disk 700
+move ..\disk\factbin.1 ..\disk\disk1
+move ..\disk\factbin.2 ..\disk\disk2
+
+echo NO split factole.z
+copy ..\disk\factole.z ..\disk\disk2
+
+echo split custctl.z
+call fsplit ..\disk\custctl.z ..\disk 1130
+move ..\disk\custctl.1 ..\disk\disk2
+move ..\disk\custctl.2 ..\disk\disk3
+
+echo NO split factdll.z
+copy ..\disk\factdll.z ..\disk\disk3
+
+echo NO split shared.z
+copy ..\disk\shared.z ..\disk\disk3
+
+
+echo split rtm.z
+call fsplit ..\disk\rtm.z ..\disk 90
+move ..\disk\rtm.1 ..\disk\disk3
+move ..\disk\rtm.2 ..\disk\disk4
+move ..\disk\rtm.3 ..\disk\disk5
+move ..\disk\rtm.4 ..\disk\disk6
+
+echo NO split factdb.z
+copy ..\disk\factdb.z ..\disk\disk6
+
+
+echo .
+echo split .z files is finished.  Run makedisk.bat to copy distribution disk.
+
+echo .
+choice /c:yn /t:y,7 Do you want to Compile the Setup Script
+if errorlevel 2 goto skipcompile
+compile setup
+echo .
+packlist setup.lst
+echo .
+
+
+:skipcompile
+call copysetf.bat
+
+echo .
+call makedisk
