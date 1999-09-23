1cd745870692ff8d9dea4dc7ed35d9950d1815bc
Davidc <davidc@localhost>
1999-09-23 13:56:00 +0000

    vss-git-convert

--- SRCCODE/(not used) INSTALLATION/C-Store Auditing/buildlog.bat
+++ SRCCODE/(not used) INSTALLATION/C-Store Auditing/buildlog.bat
@@ -3,9 +3,9 @@ echo .
 echo Updating DiskBild database
 if "%3" == "b" goto Beta
 if "%3" == "B" goto Beta
-K:\DISKBILD\DISKBILD.EXE %1 %2 csafiles.LST
+K:\DISKBILD\DISKBILD.EXE %13 %2 csafiles.LST
 goto done
 
 :Beta
-K:\DISKBILD\DISKBILD.EXE BETA%1 %2 csafiles.LST
+K:\DISKBILD\DISKBILD.EXE BETA%13 %2 csafiles.LST
 :done
