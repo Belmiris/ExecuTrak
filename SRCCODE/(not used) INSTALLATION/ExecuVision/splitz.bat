e691abfcbebd60bac4c5ff9696da00fc6f2c7140
Davidc <davidc@localhost>
1999-11-17 19:44:29 +0000

    vss-git-convert

--- SRCCODE/(not used) INSTALLATION/ExecuVision/splitz.bat
+++ SRCCODE/(not used) INSTALLATION/ExecuVision/splitz.bat
@@ -2,28 +2,30 @@
 call makedir.bat
 
 echo split factbin.z
-call fsplit ..\disk\factbin.z ..\disk 690
+call fsplit ..\disk\factbin.z ..\disk 700
 move ..\disk\factbin.1 ..\disk\disk1
 move ..\disk\factbin.2 ..\disk\disk2
 
-echo NO split factole.z
-copy ..\disk\factole.z ..\disk\disk2
+echo split factole.z
+call fsplit ..\disk\factole.z ..\disk 114
+move ..\disk\factole.1 ..\disk\disk2
+move ..\disk\factole.2 ..\disk\disk3
 
 echo split custctl.z
-call fsplit ..\disk\custctl.z ..\disk 280
-move ..\disk\custctl.1 ..\disk\disk2
-move ..\disk\custctl.2 ..\disk\disk3
-move ..\disk\custctl.3 ..\disk\disk4
+call fsplit ..\disk\custctl.z ..\disk 1392
+move ..\disk\custctl.1 ..\disk\disk3
+move ..\disk\custctl.2 ..\disk\disk4
 
-echo NO split factdll.z
-copy ..\disk\factdll.z ..\disk\disk4
+echo split factdll.z
+call fsplit ..\disk\factdll.z ..\disk 822
+move ..\disk\factdll.1 ..\disk\disk4
+move ..\disk\factdll.2 ..\disk\disk5
 
 echo split rtm.z
-call fsplit ..\disk\rtm.z ..\disk 260
-move ..\disk\rtm.1 ..\disk\disk4
-move ..\disk\rtm.2 ..\disk\disk5
-move ..\disk\rtm.3 ..\disk\disk6
-move ..\disk\rtm.4 ..\disk\disk7
+call fsplit ..\disk\rtm.z ..\disk 1114
+move ..\disk\rtm.1 ..\disk\disk5
+move ..\disk\rtm.2 ..\disk\disk6
+move ..\disk\rtm.3 ..\disk\disk7
 
 echo NO split shared.z
 copy ..\disk\shared.z ..\disk\disk7
