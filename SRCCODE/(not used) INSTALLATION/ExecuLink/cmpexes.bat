c282db91dcb7e7bef7014c366c36a73bb258baff
Davidc <davidc@localhost>
1999-11-18 17:50:48 +0000

    vss-git-convert

--- SRCCODE/(not used) INSTALLATION/ExecuLink/cmpexes.bat
+++ SRCCODE/(not used) INSTALLATION/ExecuLink/cmpexes.bat
@@ -11,7 +11,7 @@ echo Compress Application files
 
 echo .
 echo call DISKBILD.BAT
-call buildlog.bat RPPCA3 %1
+call buildlog.bat RPPCA %1
 
 echo .
 echo Compress all files in g:\program\release\execlink
@@ -19,14 +19,20 @@ echo and cpylocal.exe to factbin.z
 icomp g:\program\release\execlink\*.* ..\disk\factbin.z -i
 icomp g:\program\release\exectrak\common\cpylocal.exe ..\disk\factbin.z BIN
 
-rem echo .
-rem echo Run the PACKCALC and edit the SPLITZ.BAT/SETUP.LST
-rem echo and then RUN the SPLITZ.BAT
 echo .
-echo Z File sequence (FACTBIN.Z, CRYSTAL.Z, FACTDB.Z)
+echo Run the PACKCALC and edit the SPLITZ.BAT/SETUP.LST
+echo Z File sequence (FACTBIN.Z, SHARED.Z, FACTDLL.Z, FACTOLE.Z, CUSTCTL.Z
+echo                   , RTM.Z, FACTDB.Z)
 
 echo .
-echo RUN the SPLITZ.BAT
+echo Then run the SPLITZ.BAT to split the Z file.
+
+echo .
+choice /c:yn /t:y,7 Do you want to run the splitz.bat.
+
+if errorlevel 2 goto finished
+
+echo .
 call splitz
 
 goto finished
--- SRCCODE/(not used) INSTALLATION/ExecuLink/splitz.bat
+++ SRCCODE/(not used) INSTALLATION/ExecuLink/splitz.bat
@@ -5,31 +5,30 @@ call makedir.bat
 echo NO split factbin.z
 copy ..\disk\factbin.z ..\disk\disk1
 
-echo split shared.z
-call fsplit ..\disk\shared.z ..\disk 235
-move ..\disk\shared.1 ..\disk\disk1
-move ..\disk\shared.2 ..\disk\disk2
+echo NO split shared.z
+copy ..\disk\shared.z ..\disk\disk1
 
-echo NO split factdll.z
-copy ..\disk\factdll.z ..\disk\disk2
+echo split factdll.z
+call fsplit ..\disk\factdll.z ..\disk 191
+move ..\disk\factdll.1 ..\disk\disk1
+move ..\disk\factdll.2 ..\disk\disk2
 
 echo NO split factole.z
 copy ..\disk\factole.z ..\disk\disk2
 
 echo split custctl.z
-call fsplit ..\disk\custctl.z ..\disk 1041
+call fsplit ..\disk\custctl.z ..\disk 1266
 move ..\disk\custctl.1 ..\disk\disk2
 move ..\disk\custctl.2 ..\disk\disk3
 
 echo split rtm.z
-call fsplit ..\disk\rtm.z ..\disk 471
+call fsplit ..\disk\rtm.z ..\disk 699
 move ..\disk\rtm.1 ..\disk\disk3
 move ..\disk\rtm.2 ..\disk\disk4
 move ..\disk\rtm.3 ..\disk\disk5
-move ..\disk\rtm.4 ..\disk\disk6
 
 echo NO split factdb.z
-copy ..\disk\factdb.z ..\disk\disk7
+copy ..\disk\factdb.z ..\disk\disk5
 
 echo .
 echo split .z files is finished.  Run makedisk.bat to copy distribution disk.
