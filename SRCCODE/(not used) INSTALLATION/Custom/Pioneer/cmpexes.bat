721c3784105152ded2f06e35b3bbd7bc7320b1b1
Davidc <davidc@localhost>
1999-11-30 16:27:09 +0000

    vss-git-convert

--- SRCCODE/(not used) INSTALLATION/Custom/Pioneer/cmpexes.bat
+++ SRCCODE/(not used) INSTALLATION/Custom/Pioneer/cmpexes.bat
@@ -14,17 +14,17 @@ echo call DISKBILD.BAT
 call buildlog.bat PIONEER %1
 
 echo .
-echo Compress all files in appfiles directory to factbin.z
-icomp ..\appfiles\*.* ..\disk\factbin.z -i
-
-rem echo .
-rem echo Run the PACKCALC and edit the SPLITZ.BAT/SETUP.LST
-rem echo and then RUN the SPLITZ.BAT
-echo .
-echo Z File sequence (FACTBIN.Z, CRYSTAL.Z, FACTDB.Z)
+echo Compress all files to factbin.z
+icomp g:\program\release\custom\pioneer\*.* ..\disk\factbin.z -i
+icomp g:\program\release\exectrak\common\cpylocal.exe ..\disk\factbin.z BIN
 
 echo .
-echo RUN the SPLITZ.BAT
+echo Run the PACKCALC and edit the SPLITZ.BAT/SETUP.LST
+echo Z File sequence (FACTBIN.Z, SHARED.Z, FACTDLL.Z, FACTOLE.Z, CUSTCTL.Z
+echo                   , FACTDB.Z, CRYSTAL.Z, RTM.Z)
+
+echo .
+echo Then run the SPLITZ.BAT to split the Z file.
 call splitz
 
 goto finished
