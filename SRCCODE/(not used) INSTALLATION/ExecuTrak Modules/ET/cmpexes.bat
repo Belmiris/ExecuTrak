59709a3b9de0a6e7c7db313144e6b0627ee9fb3c
Davidc <davidc@localhost>
1999-11-30 16:28:52 +0000

    vss-git-convert

---
+++ SRCCODE/(not used) INSTALLATION/ExecuTrak Modules/ET/cmpexes.bat
@@ -0,0 +1,64 @@
+rem Compile ONLY the EXE files
+@echo off
+
+if "%1" == "" goto parm_missing
+
+echo .
+choice /c:yn /t:y,7 Do you want to COPY FILES FROM RELEASE directory
+if errorlevel 2 goto skipcopyfiles
+
+echo .
+echo Copy files from release directory
+call copyfile
+
+
+:skipcopyfiles
+
+echo .
+choice /c:yn /t:y,7 Do you want to CONTINUE the DISK BUILD PROCESS
+if errorlevel 2 goto quitbuild
+
+
+echo Delete z files
+del ..\disk\factbin.z
+rem del ..\disk\factdb.z
+
+echo .
+echo Compress Application files
+
+echo .
+echo call DISKBILD.BAT
+call buildlog.bat %1
+
+echo .
+echo Compress all files in appfiles directory to factbin.z
+icomp ..\appfiles\*.* ..\disk\factbin.z -i
+
+echo .
+echo Compress Local Database
+icomp g:\program\release\exectrak\local_db\*.* ..\disk\factdb.z
+
+rem echo .
+rem echo Run the PACKCALC and edit the SPLITZ.BAT/SETUP.LST
+rem echo and then RUN the SPLITZ.BAT
+
+echo .
+echo RUN the SPLITZ.BAT
+call splitz
+
+goto finished
+
+:parm_missing
+echo .
+echo missing or invalid parameter.  format: cmpfiles [Module Version]
+echo .
+goto finished
+
+
+:quitbuild
+echo .
+echo Disk Build Process has been cancelled.
+echo .
+
+
+:finished
