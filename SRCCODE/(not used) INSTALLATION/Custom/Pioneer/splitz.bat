270115017c72e95fe3becbad35ba32ea65b15c46
Xijian <xijian@localhost>
1999-12-17 22:49:56 +0000

    vss-git-convert

--- SRCCODE/(not used) INSTALLATION/Custom/Pioneer/splitz.bat
+++ SRCCODE/(not used) INSTALLATION/Custom/Pioneer/splitz.bat
@@ -7,25 +7,27 @@ copy ..\disk\factbin.z ..\disk\disk1
 echo NO split shared.z
 copy ..\disk\shared.z ..\disk\disk1
 
-echo NO split factdll.z
-copy ..\disk\factdll.z ..\disk\disk1
+echo split factdll.z
+call fsplit ..\disk\factdll.z ..\disk 150
+move ..\disk\factdll.1 ..\disk\disk1
+move ..\disk\factdll.2 ..\disk\disk2
 
 echo NO split factole.z
 copy ..\disk\factole.z ..\disk\disk2
 
 echo NO split custctl.z
-call fsplit ..\disk\custctl.z ..\disk 1270
+call fsplit ..\disk\custctl.z ..\disk 1220
 move ..\disk\custctl.1 ..\disk\disk2
 move ..\disk\custctl.2 ..\disk\disk3
 
 echo split crystal.z
-call fsplit ..\disk\crystal.z ..\disk 740
+call fsplit ..\disk\crystal.z ..\disk 630
 move ..\disk\crystal.1 ..\disk\disk3
 move ..\disk\crystal.2 ..\disk\disk4
 move ..\disk\crystal.3 ..\disk\disk5
 
 echo split rtm.z
-call fsplit ..\disk\rtm.z ..\disk 1020
+call fsplit ..\disk\rtm.z ..\disk 910
 move ..\disk\rtm.1 ..\disk\disk5
 move ..\disk\rtm.2 ..\disk\disk6
 move ..\disk\rtm.3 ..\disk\disk7
