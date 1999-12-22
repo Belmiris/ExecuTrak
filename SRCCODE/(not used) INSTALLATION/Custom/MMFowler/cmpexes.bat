5a5c18aadcd153b8328422065945f327da65161d
Xijian <xijian@localhost>
1999-12-22 14:14:25 +0000

    vss-git-convert

--- SRCCODE/(not used) INSTALLATION/Custom/MMFowler/cmpexes.bat
+++ SRCCODE/(not used) INSTALLATION/Custom/MMFowler/cmpexes.bat
@@ -1,33 +1,57 @@
-rem Compile ONLY the EXE files
 @echo off
+rem Note: NEED TO copy the files to APPFILES\BIN directory from \PROGRAM\RELEASE\CSTORMNT
+rem       Move the HELP FILE to APPFILES directory
 
 if "%1" == "" goto parm_missing
 
-echo Delete z files
-del ..\disk\factbin.z
-del ..\disk\factdb.z
+rem CREATE SETUP.BMP AND CHANGE VERSION
+call buildbmp.bat %1
 
-echo .
-echo Compress Application files
+echo Delete z file
+del ..\disk\factbin.z
 
 echo .
 echo call DISKBILD.BAT
 call buildlog.bat MMFOWLER %1
 
 echo .
+choice /c:yn /t:y,7 Do you want to CONTINUE the DISK BUILD PROCESS
+if errorlevel 2 goto quitbuild
+
+echo .
+echo Compress Application files
+
+echo .
 echo Compress all files in appfiles directory to factbin.z
-icomp ..\appfiles\*.* ..\disk\factbin.z -i
+icomp g:\program\release\Custom\MMFOWLER\*.* ..\disk\factbin.z -i
+
+icomp g:\program\release\exectrak\common\*.hlp ..\disk\factbin.z
+icomp g:\PROGRAM\release\exectrak\common\*.exe ..\disk\factbin.z BIN
+
+goto showinfo
+
+:quitbuild
+echo .
+echo DISK BUILD PROCESS has been cancelled
+
+goto finished
+
+:showinfo
+echo .
+echo Run the PACKCALC and edit the SPLITZ.BAT/SETUP.LST
+echo .
+echo Z File sequence (FACTBIN.Z, FACTOLE.Z, CUSTCTL.Z,
+echo                  FACTDLL.Z, RTM.Z, SHARED.Z, FACTDB.Z)
 
 echo .
-echo Compress Local Database
-icomp g:\program\release\exectrak\local_db\*.* ..\disk\factdb.z
-
-rem echo .
-rem echo Run the PACKCALC and edit the SPLITZ.BAT/SETUP.LST
-rem echo and then RUN the SPLITZ.BAT
+echo Then run the SPLITZ.BAT to split the Z file.
 
 echo .
-echo RUN the SPLITZ.BAT
+choice /c:yn /t:y,7 Do you want to run the splitz.bat.
+
+if errorlevel 2 goto finished
+
+echo .
 call splitz
 
 goto finished
--- SRCCODE/(not used) INSTALLATION/Custom/MMFowler/splitz.bat
+++ SRCCODE/(not used) INSTALLATION/Custom/MMFowler/splitz.bat
@@ -10,23 +10,23 @@ echo NO split factole.z
 copy ..\disk\factole.z ..\disk\disk2
 
 echo split custctl.z
-call fsplit ..\disk\custctl.z ..\disk 1130
+call fsplit ..\disk\custctl.z ..\disk 380
 move ..\disk\custctl.1 ..\disk\disk2
 move ..\disk\custctl.2 ..\disk\disk3
+move ..\disk\custctl.3 ..\disk\disk4
 
 echo NO split factdll.z
-copy ..\disk\factdll.z ..\disk\disk3
+copy ..\disk\factdll.z ..\disk\disk4
 
 echo NO split shared.z
-copy ..\disk\shared.z ..\disk\disk3
+copy ..\disk\shared.z ..\disk\disk4
 
 
 echo split rtm.z
-call fsplit ..\disk\rtm.z ..\disk 90
-move ..\disk\rtm.1 ..\disk\disk3
-move ..\disk\rtm.2 ..\disk\disk4
-move ..\disk\rtm.3 ..\disk\disk5
-move ..\disk\rtm.4 ..\disk\disk6
+call fsplit ..\disk\rtm.z ..\disk 700
+move ..\disk\rtm.1 ..\disk\disk4
+move ..\disk\rtm.2 ..\disk\disk5
+move ..\disk\rtm.3 ..\disk\disk6
 
 echo NO split factdb.z
 copy ..\disk\factdb.z ..\disk\disk6
