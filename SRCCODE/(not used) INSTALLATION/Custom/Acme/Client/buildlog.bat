1f4719739c0993eb150b3c37511532929a364c6f
Xijian <xijian@localhost>
2000-03-13 16:59:57 +0000

    vss-git-convert

---
+++ SRCCODE/(not used) INSTALLATION/Custom/Acme/Client/buildlog.bat
@@ -0,0 +1,17 @@
+rem %3 = BETA or Blank
+@echo off
+echo .
+echo Updating DiskBild database
+
+if "%3" == "" goto Normal_Installation
+
+K:\DISKBILD\DISKBILD.EXE BETAACME3 %2 MODFILES.LST
+
+goto done
+
+:Normal_Installation
+K:\DISKBILD\DISKBILD.EXE ACME3 %2 MODFILES.LST
+
+goto done
+
+:done
