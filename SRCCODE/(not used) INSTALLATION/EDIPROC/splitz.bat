a9c42b5b2e0b2ee7af57f644470928ec6214e071
Dchai <dchai@localhost>
2000-06-07 19:01:03 +0000

    vss-git-convert

--- SRCCODE/(not used) INSTALLATION/EDIPROC/splitz.bat
+++ SRCCODE/(not used) INSTALLATION/EDIPROC/splitz.bat
@@ -5,13 +5,30 @@ echo split custctl.z
 call fsplit ..\disk\ediproc.z ..\disk 700
 move ..\disk\ediproc.1 ..\disk\disk1
 move ..\disk\ediproc.2 ..\disk\disk2
-move ..\disk\ediproc.3 ..\disk\disk3
-move ..\disk\ediproc.4 ..\disk\disk4
-move ..\disk\ediproc.5 ..\disk\disk5
-move ..\disk\ediproc.6 ..\disk\disk6
 
-echo .
-echo split .z files is finished.  Run makedisk.bat to copy distribution disk.
+echo NO split factole.z
+copy ..\disk\factole.z ..\disk\disk2
+
+echo split custctl.z
+call fsplit ..\disk\custctl.z ..\disk 972
+move ..\disk\custctl.1 ..\disk\disk2
+move ..\disk\custctl.2 ..\disk\disk3
+
+echo NO split factdll.z
+copy ..\disk\factdll.z ..\disk\disk3
+
+echo split rtm.z
+call fsplit ..\disk\rtm.z ..\disk 202
+move ..\disk\rtm.1 ..\disk\disk3
+move ..\disk\rtm.2 ..\disk\disk4
+move ..\disk\rtm.3 ..\disk\disk5
+move ..\disk\rtm.4 ..\disk\disk6
+
+echo NO split shared.z
+copy ..\disk\shared.z ..\disk\disk6
+
+echo No split factdb.z
+copy ..\disk\factdb.z ..\disk\disk6
 
 echo .
 choice /c:yn /t:y,7 Do you want to Compile the Setup Script
