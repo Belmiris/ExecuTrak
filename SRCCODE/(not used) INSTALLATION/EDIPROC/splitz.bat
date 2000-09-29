81a6f269ecb2e7aba2ea17858a1368ebbb8d2ce1
Dchai <dchai@localhost>
2000-09-29 21:46:39 +0000

    vss-git-convert

--- SRCCODE/(not used) INSTALLATION/EDIPROC/splitz.bat
+++ SRCCODE/(not used) INSTALLATION/EDIPROC/splitz.bat
@@ -10,7 +10,7 @@ echo NO split factole.z
 copy ..\disk\factole.z ..\disk\disk2
 
 echo split custctl.z
-call fsplit ..\disk\custctl.z ..\disk 971
+call fsplit ..\disk\custctl.z ..\disk 933
 move ..\disk\custctl.1 ..\disk\disk2
 move ..\disk\custctl.2 ..\disk\disk3
 
@@ -18,7 +18,7 @@ echo NO split factdll.z
 copy ..\disk\factdll.z ..\disk\disk3
 
 echo split rtm.z
-call fsplit ..\disk\rtm.z ..\disk 201
+call fsplit ..\disk\rtm.z ..\disk 163
 move ..\disk\rtm.1 ..\disk\disk3
 move ..\disk\rtm.2 ..\disk\disk4
 move ..\disk\rtm.3 ..\disk\disk5
